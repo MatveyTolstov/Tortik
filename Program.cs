f04816a7c1c89fc6ff30c0298d274a04e4cb30a7
PETRFSOFTWARE <144270275+MatveyTolstov@users.noreply.github.com>
2023-10-31 14:05:32 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,13 +4,14 @@ namespace Добрый_магазин_тортиков
 {
     internal class Program
     {
-            static string zakaz = "";
-            static int price = 0;
+        static string zakaz = "";
+        static int price = 0;
         static void Main()
         {
-            while (true) 
+            while (true)
             {
 
+                Console.Clear();
                 Console.WriteLine("Заказ тортов в магазине Добрый Отец, покупайте! ");
                 Console.WriteLine("Выберите параметр торта");
                 Console.WriteLine("---------------------------------------------------------------------------------------------");
@@ -41,7 +42,6 @@ namespace Добрый_магазин_тортиков
                     if (key.Key == ConsoleKey.Escape)
                     {
                         Console.Clear();
-                        Main();
                     }
                     pos = menu.Show(3, 6);
                     if (pos == 3)
@@ -49,7 +49,6 @@ namespace Добрый_магазин_тортиков
                         Console.Clear();
                         price += 500;
                         zakaz = zakaz + " Квадрат - 500";
-                        Main();
 
                     }
                     else if (pos == 4)
@@ -57,25 +56,21 @@ namespace Добрый_магазин_тортиков
                         Console.Clear();
                         price += 300;
                         zakaz = zakaz + " Круг -- 300";
-                        Main();
-
                     }
                     else if (pos == 5)
                     {
                         Console.Clear();
                         price += 200;
                         zakaz = zakaz + " Сердечко -- 200";
-                        Main();
-
                     }
                     else if (pos == 6)
                     {
                         Console.Clear();
                         price += 340;
                         zakaz = zakaz + " Прямоугольник -- 340";
-                        Main();
                     }
                     continue;
+
                 }
                 if (pos == 4)
                 {
@@ -90,7 +85,6 @@ namespace Добрый_магазин_тортиков
                     if (key.Key == ConsoleKey.Escape)
                     {
                         Console.Clear();
-                        Main();
                     }
                     pos = menu.Show(3, 5);
                     if (pos == 3)
@@ -98,7 +92,6 @@ namespace Добрый_магазин_тортиков
                         Console.Clear();
                         price += 1000;
                         zakaz = zakaz + "Маленький (Диаметр - 16 см, 8 порций) - 1000";
-                        Main();
 
                     }
                     else if (pos == 4)
@@ -106,14 +99,12 @@ namespace Добрый_магазин_тортиков
                         Console.Clear();
                         price += 1200;
                         zakaz = zakaz + "Обычный (Диаметр - 18 см, 10 порций) - 1200";
-                        Main();
                     }
                     else if (pos == 5)
                     {
                         Console.Clear();
                         price += 2000;
                         zakaz = zakaz + "Большой (Диаметр - 28 см, 24 порции) - 2000";
-                        Main();
                     }
                     continue;
 
@@ -133,7 +124,7 @@ namespace Добрый_магазин_тортиков
                     if (key.Key == ConsoleKey.Escape)
                     {
                         Console.Clear();
-                        Main();
+
                     }
                     pos = menu.Show(3, 7);
                     if (pos == 3)
@@ -141,7 +132,7 @@ namespace Добрый_магазин_тортиков
                         Console.Clear();
                         price += 100;
                         zakaz = zakaz + "Ванильный - 100";
-                        Main();
+
 
                     }
                     else if (pos == 4)
@@ -149,27 +140,27 @@ namespace Добрый_магазин_тортиков
                         Console.Clear();
                         price += 100;
                         zakaz = zakaz + "Шоколадный - 100";
-                        Main();
+
                     }
                     else if (pos == 5)
                     {
                         Console.Clear();
                         price += 150;
                         zakaz = zakaz + "Карамельный - 150";
-                        Main();
+
 
                     }
                     else if (pos == 6)
                     {
                         price += 200;
                         zakaz = zakaz + "Ягодный - 200";
-                        Main();
+
                     }
                     else if (pos == 7)
                     {
                         price += 250;
                         zakaz = zakaz + "Кокосовый - 250";
-                        Main();
+
 
 
                     }
@@ -189,7 +180,7 @@ namespace Добрый_магазин_тортиков
                     if (key.Key == ConsoleKey.Escape)
                     {
                         Console.Clear();
-                        Main();
+
                     }
                     pos = menu.Show(3, 5);
                     if (pos == 3)
@@ -197,7 +188,7 @@ namespace Добрый_магазин_тортиков
                         Console.Clear();
                         price += 200;
                         zakaz = zakaz + "1 корж - 200";
-                        Main();
+
 
                     }
                     else if (pos == 4)
@@ -205,21 +196,21 @@ namespace Добрый_магазин_тортиков
                         Console.Clear();
                         price += 400;
                         zakaz = zakaz + "2 коржа - 400";
-                        Main();
+
                     }
                     else if (pos == 5)
                     {
                         Console.Clear();
                         price += 600;
                         zakaz = zakaz + "3 коржа - 600";
-                        Main();
+
                     }
                     else if (pos == 6)
                     {
                         Console.Clear();
                         price += 800;
                         zakaz = zakaz + "4 коржа - 800";
-                        Main();
+
                     }
                     continue;
                 }
@@ -243,15 +234,15 @@ namespace Добрый_магазин_тортиков
                     if (key.Key == ConsoleKey.Escape)
                     {
                         Console.Clear();
-                        Main();
+
                     }
                     pos = menu.Show(3, 10);
-                     if (pos == 3)
+                    if (pos == 3)
                     {
                         Console.Clear();
                         price += 100;
                         zakaz = zakaz + "Шоколад - 100";
-                        Main();
+
 
                     }
                     else if (pos == 4)
@@ -259,49 +250,49 @@ namespace Добрый_магазин_тортиков
                         Console.Clear();
                         price += 100;
                         zakaz = zakaz + "Крем - 100";
-                        Main();
+
                     }
                     else if (pos == 5)
                     {
                         Console.Clear();
                         price += 150;
                         zakaz = zakaz + "Бизе - 150";
-                        Main();
+
                     }
                     else if (pos == 6)
                     {
                         Console.Clear();
                         price += 150;
                         zakaz = zakaz + "Драже -150";
-                        Main();
+
                     }
                     else if (pos == 7)
                     {
                         Console.Clear();
                         price += 200;
                         zakaz = zakaz + "Ягоды - 200";
-                        Main();
+
                     }
                     else if (pos == 8)
                     {
                         Console.Clear();
                         price += 150;
                         zakaz = zakaz + "Шоколадная - 150";
-                        Main();
+
                     }
                     else if (pos == 9)
                     {
                         Console.Clear();
                         price += 150;
                         zakaz = zakaz + "Ягодная - 150";
-                        Main();
+
                     }
                     else if (pos == 10)
                     {
                         Console.Clear();
                         price += 150;
                         zakaz = zakaz + "Кремовая - 150";
-                        Main();
+
                     }
                     continue;
 
@@ -319,7 +310,7 @@ namespace Добрый_магазин_тортиков
                     if (key.Key == ConsoleKey.Escape)
                     {
                         Console.Clear();
-                        Main();
+
                     }
                     pos = menu.Show(3, 10);
                     if (pos == 3)
@@ -327,7 +318,7 @@ namespace Добрый_магазин_тортиков
                         Console.Clear();
                         price += 150;
                         zakaz = zakaz + "Шоколад - 150";
-                        Main();
+
 
                     }
                     else if (pos == 4)
@@ -335,33 +326,33 @@ namespace Добрый_магазин_тортиков
                         Console.Clear();
                         price += 150;
                         zakaz = zakaz + "Ягодная - 150";
-                        Main();
+
                     }
                     else if (pos == 5)
                     {
                         Console.Clear();
                         price += 150;
                         zakaz = zakaz + "Кремовая - 150";
-                        Main();
+
                     }
                     continue;
 
                 }
                 if (pos == 9)
                 {
-                    Console.Clear ();
+                    Console.Clear();
                     File.AppendAllText("заказ.txt", "\n Заказ от " + DateTime.Now);
                     File.AppendAllText("заказ.txt", $"\n Заказ: {zakaz}");
                     File.AppendAllText("заказ.txt", $"\n Цена: {price}");
                     break;
-                    
+
                 }
 
 
-            } 
+            }
 
 
-            
+
 
 
 
